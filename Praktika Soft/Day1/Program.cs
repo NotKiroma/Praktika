744c4c2a052ebbc2a48bd866af71ad6307430046
Егор <kiromachannel@gmail.com>
2024-11-05 15:00:16 +0500

    Bu

---
+++ Praktika Soft/Day1/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}!!!");
+    }
+}
---
+++ Praktika Soft/Day2/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 8;
+        int b = 2;
+        int result = 8 * 2;
+        Console.WriteLine(result);
+    }
+}
